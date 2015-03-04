897142204eba810a8e373b7808b112ff4e3289ff
Mark Nunberg <mnunberg@haskalah.org>
2015-03-04 11:35:21 -0500

    Updated license year. Add copyright references to source files

--- jsonsl.c
+++ jsonsl.c
@@ -1,3 +1,8 @@
+/* Copyright (C) 2012-2015 Mark Nunberg.
+ *
+ * See included LICENSE file for license details.
+ */
+
 #include "jsonsl.h"
 #include <assert.h>
 #include <limits.h>
--- jsonsl.h
+++ jsonsl.h
@@ -4,6 +4,9 @@
  * - Maintains state
  * - Callback oriented
  * - Lightweight and fast. One source file and one header file
+ *
+ * Copyright (C) 2012-2015 Mark Nunberg
+ * See included LICENSE file for license details.
  */
 
 #ifndef JSONSL_H_
